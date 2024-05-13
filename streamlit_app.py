9e31ec5c80ca5f59e9b5531373453733c0a0647c
Alana Gorukanti <52054877+alana170@users.noreply.github.com>
2024-05-13 14:45:08 -0400

    connecting to fruit api

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,29 +22,22 @@ ingredients_list = st.multiselect('Choose up to 5 ingredients: ',
                                   max_selections = 5)
 
 if ingredients_list:
-    #st.write(ingredients_list)
-    #st.text(ingredients_list)
-    ingredients_string = ""
+    ingredients_string = ''
+
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-    #st.write(ingredients_string)
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                 values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
-    
-    #st.write(my_insert_stmt)
-    #st.stop()
     
     time_to_insert = st.button("Submit Order")
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order+ '!', icon="✅")
 
-
-# New section to display fruityvice nutrition information import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
 
 
 
